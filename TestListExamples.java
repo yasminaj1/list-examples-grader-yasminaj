65c84f6848aa87c8b13d25427ac044ebdcd5d409
yasminaj1 <114477341+yasminaj1@users.noreply.github.com>
2022-11-26 19:58:23 -0800

    test first timem

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,10 +1,106 @@
+// // import static org.junit.Assert.*;
+// // import org.junit.*;
+// // import java.util.*;
+
+// import static org.junit.Assert.*;
+// import org.junit.*;
+// import java.util.ArrayList;
+// import java.util.List;
+
+// class CheckString implements StringChecker {
+//     @Override
+//     public boolean checkString(String s) {
+//         if(s.charAt(0) == 'a') {
+//             return true;
+//         }
+//         return false;
+//     }
+// }
+
+// public class TestListExamples {
+//   // @Test(timeout = 100)
+//   // public void testTimeout() {
+//   //   while(true) {}
+//   // }
+//   // Write your grading tests here!
+
+
+//   @Test
+//     public void testFilerMethod1(){
+//         //StringChecker sChecker1 = new StringChecker2();
+//         List<String> input1 = new ArrayList<>();
+//         input1.add("plated");
+//         input1.add("edward");
+//         input1.add("hands");
+//         input1.add("fasted");
+//         input1.add("places");
+//         List<String> output2 = new ArrayList<>();
+//         output2.add("plated");
+//         output2.add("edward");
+//         output2.add("fasted");
+//         StringChecker sChecker1 = new StringChecker2();
+//         List<String> filteredList = ListExamples.filter(input1, sChecker1);
+//         assertArrayEquals(output2,filteredList);
+//     }
+
+//     @Test
+//     public void testMergeMethod1(){
+//       List<String> listToMerge1 = new ArrayList<String>();
+//       List<String> listToMerge2 = new ArrayList<String>();
+//       listToMerge1.add("ape");
+//       listToMerge1.add("zebra");
+//       listToMerge1.add("crocodile");
+//       listToMerge1.add("walrus");
+//       listToMerge2.add("elephant");
+//       listToMerge2.add("bird");
+//       listToMerge2.add("spider");
+//       List<String> mergedList = ListExamples.merge(listToMerge1, listToMerge1);
+//       String[] expectedOutcome = {"ape","bird","crocodile","elephant","spider","walrus","zebra"};
+//       assertArrayEquals(expectedOutcome, mergedList.toArray());
+//   }
+//     }
+
+
+
+
+// //}
+
+
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
+
+class CheckString implements StringChecker {
+    @Override
+    public boolean checkString(String s) {
+        if(s.charAt(0) == 'a') {
+            return true;
+        }
+        return false;
+    }
+}
 
 public class TestListExamples {
-  @Test(timeout = 100)
-  public void testTimeout() {
-    while(true) {}
-  }
-  // Write your grading tests here!
+  @Test
+    public void checkFilter() {
+        CheckString c = new CheckString();
+        List<String> list = new ArrayList<>();
+        list.add("apple"); list.add("banana"); list.add("apricot");
+        List<String> list2 = new ArrayList<>();
+        list2.add("apple"); list2.add("apricot");
+        assertEquals(ListExamples.filter(list,c), list2);
+    }
+
+    @Test
+    public void checkMerge() {
+        List<String> list1 = new ArrayList<>();
+        list1.add("apple"); list1.add("banana"); list1.add("duplex");
+        List<String> list2 = new ArrayList<>();
+        list2.add("cucumber"); list2.add("ergonomics"); list2.add("france");
+        List<String> list3 = new ArrayList<>();
+        list3.add("apple"); list3.add("banana"); list3.add("cucumber"); list3.add("duplex");
+        list3.add("ergonomics"); list3.add("france");
+        assertEquals(ListExamples.merge(list1, list2), list3);
+    }
 }
--- grade.sh
+++ grade.sh
@@ -1,9 +1,57 @@
 # Create your grading script here
 
+set -e
+
+CPATH=".:hamcrest-core-1.3.jar:junit-4.13.2.jar"
+
+#clone student submission
+
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
 
+cd student-submission/
 
+if[ -e ListExamples.java ]
+then 
+    echo "file found"
+    echo "recieve 1 point"
 
+else 
+    echo "file not found in directory"
+    echo "no points recieved"
+    exit
 
+fi
+
+cp lib/hamcrest-core-1.3.jar student-submission
+cp lib/junit-4.13.2.jar student-submission
+cp TestListExamples.java student-submission
+
+cd student-submission
+
+javac ListExamples.java 2> error.txt > output.txt 
+if [ $? -eq 0 ]
+then
+    javac -cp $CPATH TestListExamples.java 2> error.txt > output.txt
+    echo "the file you have submitted (ListExamples.java) has been compiled!"
+    echo "recieve 1 point"
+    echo "total points = 2/3"
+else
+    echo "the file you have submitted (ListExamples.java) can't be compiled"
+    echo "total points = 1/3"
+    exit
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples 2> error2.txt > output.txt
+if [ $? -eq 0 ]
+then
+    echo "All tests have been passed!"
+    echo "recieve 1 point"
+    echo "total points = 3/3! Congratulations!"
+    exit
+else
+    echo "You have failed one or more tests"
+    echo "total points = 2/3"
+    exit
+fi
